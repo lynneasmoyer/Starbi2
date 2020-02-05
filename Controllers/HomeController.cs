fd85f53e2a9b8428cc15b8fce5a839c0fa37d5f0
Lynnea Smoyer <lynneasmoyer@gmail.com>
2020-02-04 22:11:00 -0600

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Starbie.Models;
+
+namespace Starbie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("About")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [HttpGet("Characters")]
+        public IActionResult Characters()
+        {
+            return View();
+        }
+
+        [HttpGet("PlayGame")]
+        public IActionResult PlayGame()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
